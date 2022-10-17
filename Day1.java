4c189e64e38e03aa6e019b36806a86645ef71a53
salin <mursalin9040@gmail.com>
2022-10-17 20:52:19 +0800

    Tipe data

---
+++ Day1.java
@@ -0,0 +1,21 @@
+package Ngoding100day;
+
+public class Day1 {
+    public static void main(String[] args) {
+        String nama,alamat;
+        int umur;
+        double tinggi_badan;
+        
+        nama = "Mursalin";
+        alamat = "Majene";
+        umur = 19;
+        tinggi_badan = 155.9;
+        
+        System.out.println("Nama         :"+nama);
+        System.out.println("Alamat       :"+alamat);
+        System.out.println("Umur         :"+umur+" Tahun");
+        System.out.println("Tinggi Badan :"+tinggi_badan+" cm");
+        
+    }
+    
+}
