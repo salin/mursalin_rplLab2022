f1d9ee766c8a7e13c579b89a0dabe028a8417e66
salin <mursalin9040@gmail.com>
2022-10-20 20:14:03 +0800

    Operasi Aritmatika

---
+++ Day4.java
@@ -0,0 +1,18 @@
+package Ngoding100day;
+
+public class Day4 {
+    public static void main(String[] args) {
+        int a,b,c,d;
+        a = 4 * 8;
+        b = 4 + 8;
+        c = 4 - 8;
+        d = 4 / 8;
+        
+        System.out.println("4 * 8 ="+a);
+        System.out.println("4 + 8 ="+b);
+        System.out.println("4 - 8 ="+c);
+        System.out.println("4 / 8 ="+d);
+        
+    }
+    
+}
