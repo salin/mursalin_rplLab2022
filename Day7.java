826b28e97432af09aaf01b9cedef4263da5a7c2d
salin <mursalin9040@gmail.com>
2022-10-23 22:57:11 +0800

    Tipe Data

---
+++ Day7.java
@@ -0,0 +1,11 @@
+package Ngoding100day;
+public class Day7 {
+    public static void main(String[] args) {
+        boolean A = true;
+        boolean B = false;
+        
+        System.out.println(A);
+        System.out.println(B);
+    }
+    
+}
