e341f2d07732139fab503b1b44816b1568ead78c
salin <mursalin9040@gmail.com>
2022-10-28 21:44:07 +0800

    Break

---
+++ Day12.java
@@ -0,0 +1,11 @@
+package Ngoding100day;
+public class Day12 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
