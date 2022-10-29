67967280faed8571eef072304ce128fea127482e
salin <mursalin9040@gmail.com>
2022-10-29 20:47:12 +0800

    Array

---
+++ Day13.java
@@ -0,0 +1,12 @@
+package Ngoding100day;
+
+public class Day13 {
+    public static void main(String[] args) {
+        String[] cars = {"Pajero", "BMW", "Ferarri"};
+        cars[0] = "Rush";
+        cars[1] = "Avanza";
+        System.out.println(cars[1]);
+        System.out.println(cars[0]);
+    }
+    
+}
