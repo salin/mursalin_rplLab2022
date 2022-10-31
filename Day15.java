82d53ff86276cbdbf43dea8b2738730c307de5f2
salin <mursalin9040@gmail.com>
2022-10-31 21:29:09 +0800

    Flor And Ceiling

---
+++ Day15.java
@@ -0,0 +1,14 @@
+package Ngoding100day;
+import java.util.Scanner;
+public class Day15 {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        double d1 = userInput.nextDouble();
+        System.out.print("Ceiling  =" + Math.ceil(d1));
+        double d2 = userInput.nextDouble();
+        System.out.print("Floor  =" + Math.floor(d2));        
+ }
+    
+}
+
+
