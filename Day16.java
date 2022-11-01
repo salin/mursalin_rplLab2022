7d2533bcb9d66c5511b8fb09447c9bb04d554e3a
salin <mursalin9040@gmail.com>
2022-11-01 19:08:41 +0800

    Angka Teracak

---
+++ Day16.java
@@ -0,0 +1,10 @@
+package Ngoding100day;
+import java.lang.Math;
+public class Day16 {
+   public static void main(String args[]) { 
+       System.out.println("1st Random Number: " + Math.random());   
+       System.out.println("2nd Random Number: " + Math.random());  
+       System.out.println("3rd Random Number: " + Math.random());    
+    }   
+}    
+
