9bc5f72a81f5d23fe88346301506eb8dc0a8355b
salin <mursalin9040@gmail.com>
2022-11-02 21:12:26 +0800

    Do While

---
+++ Day17.java
@@ -0,0 +1,11 @@
+package Ngoding100day;
+
+public class Day17 {
+    public static void main(String[] args) {
+        int batas = 0;
+        do {
+            System.out.print(batas);
+            batas++;
+        }while (batas<10);
+    }
+}
