4e8873450912302489f0ac98e63e93dca821ebea
salin <mursalin9040@gmail.com>
2022-11-03 21:35:35 +0800

    Membuat Pola Segitiga

---
+++ Day18.java
@@ -0,0 +1,14 @@
+package Ngoding100day;
+
+public class Day18 {
+    public static void main(String[] args) {
+       int a = 5;
+       for (int b = 1; b <= a; b++){
+           for (int c = 1; c <= b; c++) {
+               System.out.print('*');
+           }
+           System.out.println();
+       }
+    }
+    
+}
