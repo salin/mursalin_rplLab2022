4cd779a668ce5f65382e5ead880cd1dcab196aac
salin <mursalin9040@gmail.com>
2022-11-04 19:52:35 +0800

    Continue

---
+++ Day19.java
@@ -0,0 +1,12 @@
+package Ngoding100day;
+public class Day19 {
+    public static void main(String[] args) {
+        for (int angka = 0; angka < 8; angka++) {
+            if (angka == 5) {
+                continue;
+            }
+            System.out.println("angka ke-" + angka);
+        }
+    }
+    
+}
