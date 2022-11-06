ff38442ad9d3a68c4ef1d4ae89b51973ab6c9e40
salin <mursalin9040@gmail.com>
2022-11-06 20:02:00 +0800

    Array 2D

---
+++ Day21.java
@@ -0,0 +1,16 @@
+package Ngoding100day;
+
+public class Day21 {
+    public static void main(String[] args) {
+    //Deklarasi array
+    String[][]huruf = {
+        {"A","B","C"},
+        {"D","E","F"},
+        {"G","H","I"} 
+    };
+    //Menampilkan isi array pada indeks ke-[0][0], [1][1], dan [2][1]
+    System.out.println(huruf[0][0]);
+    System.out.println(huruf[1][1]);
+    System.out.println(huruf[2][1]);  
+  }
+}
