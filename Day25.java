6dab1f6746db828be7407fca51291109c67e7b26
salin <mursalin9040@gmail.com>
2022-11-10 22:08:56 +0800

    Class Atribut

---
+++ Day25.java
@@ -0,0 +1,13 @@
+package Ngoding100day;
+
+public class Day25 {
+int x;
+
+
+public static void main(String[] args) {
+Day25 myObj = new Day25();
+myObj.x = 40;
+System.out.println(myObj.x);
+}
+}    
+
