bd97d9d4984117dcb266628fe04ede47ee89c169
salin <mursalin9040@gmail.com>
2022-11-13 20:13:32 +0800

    Logika NOT

---
+++ Day28.java
@@ -0,0 +1,9 @@
+package Ngoding100day;
+public class Day28 {
+    public static void main(String[] args) {
+        boolean a = true ;
+        boolean b = false;
+        boolean hasil = !a; //jika a bernilai true maka hasilnya akan false begitupun sebaliknya
+        System.out.println("hasil : " + hasil);
+    }
+}
