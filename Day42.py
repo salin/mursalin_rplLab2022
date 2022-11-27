ec90b9f538074f76d59342eca4dc3a62a571d6c6
salin <99964967+salin@users.noreply.github.com>
2022-11-27 21:55:30 +0800

    Membuat dan Menampilkan List

---
+++ Day42.py
@@ -0,0 +1,12 @@
+perlengkapan = ["polpen", "buku", "hp", "laptop"]
+print(perlengkapan)
+
+print()
+for data in perlengkapan:
+    print(data)
+
+print()
+i = 0
+while i < 4:
+    print(perlengkapan[i])
+    i += 1
