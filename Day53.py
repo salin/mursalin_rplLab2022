b4842b145a20b7e3608a645289aaea73dbc01234
salin <99964967+salin@users.noreply.github.com>
2022-12-08 20:19:41 +0700

    Menghapus Data

---
+++ Day53.py
@@ -0,0 +1,8 @@
+lebar = [2, 3, 45, 56, 66]
+print("sebelum di hapus : ", lebar)
+
+  #hapus elemen yang bernilai 45 pada variabel lebar
+lebar.remove(45)
+ 
+#tampilkan nilai variabel lebar setelah penghapusan
+print("setelah di hapus : ", lebar)
