7a2bca7b29fed912aa9dac414fb1f6f0a5855b6f
salin <99964967+salin@users.noreply.github.com>
2022-12-09 21:25:02 +0700

    Perulangan Tak Terbatas

---
+++ Day54.py
@@ -0,0 +1,11 @@
+while True:
+  jawaban = int(input('\nMasukkan angka: '))
+
+  if jawaban == angka_rahasia:
+    print('Selamat, tebakanmu benar!')
+    break # berhenti paksa
+  else:
+    print(
+      'Tebakanmu terlalu',
+      'kecil' if jawaban < angka_rahasia else 'besar'
+    )
