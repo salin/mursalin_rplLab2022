a1ee5888d7c2137b609a87d463f7eb804a8399e9
salin <99964967+salin@users.noreply.github.com>
2022-12-13 22:08:36 +0700

    Menemukan Faktor

---
+++ Day58.py
@@ -0,0 +1,7 @@
+def print_faktor(x):
+# “””Fungsi menerima input bilangan dan mencetak faktornya”””
+
+    print("Faktor dari",x, "adalah :")
+    for i in range(1, x+1):
+        if x % i == 0:
+         print(i)
