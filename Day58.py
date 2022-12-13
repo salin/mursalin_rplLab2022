47444c6cc1baf1aa983d9ef562897b7e1dda5abf
salin <99964967+salin@users.noreply.github.com>
2022-12-13 23:09:40 +0800

    Delete Day58.py

--- Day58.py
+++
@@ -1,7 +0,0 @@
-def print_faktor(x):
-# “””Fungsi menerima input bilangan dan mencetak faktornya”””
-
-    print("Faktor dari",x, "adalah :")
-    for i in range(1, x+1):
-        if x % i == 0:
-         print(i)
