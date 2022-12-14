05ba99abd1b9919c8cbf686de852a12ac7e3ba94
salin <99964967+salin@users.noreply.github.com>
2022-12-14 22:10:28 +0800

    Membalik List

---
+++ Day59.py
@@ -0,0 +1,10 @@
+satu = int(input("MASUKKAN ANGKA : "))
+dua = int(input("MASUKKAN ANGKA : "))
+tiga = int(input("MASUKKAN ANGKA : "))
+empat = int(input("MASUKKAN ANGKA : "))
+lima = int(input("MASUKKAN ANGKA : "))
+
+madina = [satu, dua, tiga, empat, lima]
+print("sebelum di balik : ", lin)
+madina.reverse()
+print("setelah di balik : ", lin)
