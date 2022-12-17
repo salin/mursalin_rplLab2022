7a559503542e385ee5a4a886fd6308a98c97f521
salin <99964967+salin@users.noreply.github.com>
2022-12-17 22:38:14 +0800

    Dictionary

---
+++ Day62.py
@@ -0,0 +1,8 @@
+identitas = {
+    "nama" : "mursalin",
+    "umur" : 19,
+    "alamat" : "Majene"}
+
+print("identitas : ", identitas)
+print(identitas["nama"], "berumur", identitas["umur"])
+print(identitas.get("nama")) 
