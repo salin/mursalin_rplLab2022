addbdc432a308ca3579119a26f493989c0d38cbe
salin <99964967+salin@users.noreply.github.com>
2022-12-22 00:00:53 +0800

    Assigment

---
+++ Day66.py
@@ -0,0 +1,19 @@
+#OPERATOR ASSIGMANT
+#Operasi yang dapat dilakukan dengan penyingkatan
+
+a = 5 #Ini adalah assigmant 
+print("Nilai a = ", a)
+#Penjumlahan
+a += 1 #Artinya adalah a = a + 1
+print("Nilai a += 1, nilai a menjadi ", a)
+#Pengurangan
+a -= 3
+print("Nilai a -= 3, nilai a menjadi ", a)
+a *= 2
+print("Nilai a *= 2, nilai a menjadi ", a)
+#Pembagian
+a /= 2
+print("Nilai a /= 2, nilai a menjadi ", a)
+#Sisa Bagi
+a %= 2
+print("Nilai a %= 2, nilai a menjadi ", a)
