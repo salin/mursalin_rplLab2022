a8270094815d3bd2182551d5cb1f9720f2122fcf
salin <99964967+salin@users.noreply.github.com>
2022-12-23 23:27:07 +0800

    Or and not

---
+++ Day68.py
@@ -0,0 +1,13 @@
+
+print("==NOT==")
+a = False
+c = not a
+print("Data a =", a)
+print("================NOT")
+print ("data c =", c)
+
+print("====OR====")
+a = False
+b = False
+c = a or b
+print(a,"OR",b,"=",c)
