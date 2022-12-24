0c5faf90d47644d8e8b3f735d47702cbceb13bfe
salin <99964967+salin@users.noreply.github.com>
2022-12-24 23:06:40 +0800

    Matrix

---
+++ Day69.py
@@ -0,0 +1,9 @@
+#Matriks dengan ukuran 2x2
+matriksA = [[1,0], [0,1]]
+print(matriksA)
+#Matriks dengan ukuran 3x3
+matriksB = [[1,0,1], [0,1,0], [1,0,1]]
+print(matriksB)
+#Matriks dengan ukuran 4x4
+matriksC = [[1,0,1,0], [0,1,0,1], [1,0,1,0], [0,1,0,1]]
+print(matriksC)
