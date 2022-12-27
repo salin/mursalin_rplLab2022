486ca4f8508208d131145687d9da2f40f17851c1
salin <99964967+salin@users.noreply.github.com>
2022-12-27 22:40:18 +0800

    Menghitung Diskon Jumlah pembelian Semen

--- Day72.py
+++ Day72.py
@@ -1,14 +1,14 @@
 total = int(input('masukan jumlah semen : '))
-setelah_diskon = total
 total1 = total * 60000
- 
+setelah_diskon = total
+setelah_diskon = total1 - total 
+
 if total >= 100:
     diskon = total * (2/100)
+    print ("Anda Dikenakan Diskon 2%",(setelah_diskon))
 elif total >= 200:
     diskon = total * (5/100)
-else:
-    print("tidak ada diskon")
-    
-setelah_diskon = total1 - total 
-print("Diskonya yaitu: ",(diskon))
-print ("diskonnya adalah",(setelah_diskon))
+    print ("Anda Dikenakan Diskon 5%",(setelah_diskon))
+else :
+    print ("tidak ada diskon",(total1))
+
