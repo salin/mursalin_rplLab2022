85054ec0874a5761a7b9fd4343a0c4a66e435267
salin <99964967+salin@users.noreply.github.com>
2022-12-30 23:31:29 +0800

    Membuat Class

---
+++ Day75.py
@@ -0,0 +1,9 @@
+class bangunruang():
+    def luaspersegi():
+        print("====== Luas Persegi ======")
+        panjang = int(input("masukkan panjang : "))
+        lebar = int(input("masukkan lebar : "))
+        persegi = panjang * lebar
+        print("luas persegi : ", persegi)
+
+bangunruang.luaspersegi()
