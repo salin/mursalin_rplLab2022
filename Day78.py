f72b0cc153ca353171649eecfd7a7867b72aec65
salin <99964967+salin@users.noreply.github.com>
2023-01-02 21:38:34 +0800

    Menghitung Harga Tanah Permeter

---
+++ Day78.py
@@ -0,0 +1,12 @@
+#menghitung harga permeter tanah
+harga  =  int ( input ( "Masukkan Harga Tanah : " ))
+
+luas_tanah  =  float ( input ( "Masukkan Luas Tanah : " ))
+
+harga_permeter  =  harga  /  luas_tanah
+cetak ()
+
+print ( "<====> Informasi Harga Tanah <====>" )
+print ( "Harga : " , "Rp." , harga )
+print ( "Luas Tanah : " , luas_tanah , "m²" )
+print ( "Harga_permeter : " , "Rp." , harga_permeter )
