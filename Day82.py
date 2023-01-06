40e0e464c4d6943fb95d96726ef2887cd6a1be62
salin <99964967+salin@users.noreply.github.com>
2023-01-06 21:37:27 +0800

    Format String

---
+++ Day82.py
@@ -0,0 +1,16 @@
+#FORMAT STRING
+#CONTOH GENERIC
+#CONTOH STRING
+nama = "Kaco"
+format_str = f"haii {nama}"
+print(format_str)
+
+#ANGKA
+angka = 2003.5
+format_str = f"angka = {angka}"
+print(format_str)
+
+#BOOLEAN
+boolean = True
+format_str = f"boolean = {boolean}"
+print(format_str)
