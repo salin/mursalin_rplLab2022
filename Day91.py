34a913ba567becaa6ef4701f94c292af528337f3
salin <99964967+salin@users.noreply.github.com>
2023-01-15 11:51:30 +0800

    Menentukan Jarak

---
+++ Day91.py
@@ -0,0 +1,8 @@
+#menghitung jarak
+
+tempat_awal = int(input("Masukkan tempat awal : "))
+tempat_akhir = int(input("Masukkan tempat akhir : "))
+
+jarak = tempat_akhir - tempat_awal
+
+print("Jarak antara tempat awal dan akhir adalah : ", jarak, "KM")
